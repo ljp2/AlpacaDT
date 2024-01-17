5edee35e5fefaadcc71a5f0a559dabf80fbc0f96
ljp2 <ljp2@lehigh.edu>
2024-01-17 15:39:14 -0500

    working

--- alpaca-api.py
+++ alpaca-api.py
@@ -1,28 +1,33 @@
+from keys import paper_apikey, paper_secretkey
+import pandas as pd
+from datetime import datetime
+
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import GetAssetsRequest
 from alpaca.trading.enums import AssetClass
 
 from alpaca.trading.client import TradingClient
 from alpaca.trading.enums import OrderSide, TimeInForce
-
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.requests import CryptoLatestQuoteRequest
+from alpaca.data.requests import CryptoBarsRequest, CryptoLatestQuoteRequest
 
-from keys import paper_apikey, paper_secretkey
+apikey = paper_apikey
+secretkey = paper_secretkey
 
 def getAccountDetails():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     account = trading_client.get_account()
     return account
 
 def getAssets():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     search_params = GetAssetsRequest(asset_class=AssetClass.CRYPTO)
     assets = trading_client.get_all_assets(search_params)
     return assets  
 
 def marketOrderData(symbol, qty, side):
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     order = trading_client.submit_order(
         symbol=symbol,
         qty=qty,
@@ -33,7 +38,7 @@ def marketOrderData(symbol, qty, side):
     return order
 
 def limitOrderData(symbol, qty, side, limit_price):
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     order = trading_client.submit_order(
         symbol=symbol,
         qty=qty,
@@ -62,47 +67,68 @@ def submitLimitSellOrder(symbol, qty, limit_price):
     return order
 
 def getAllOpenOrders():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     orders = trading_client.get_orders()
     return orders
 
 def getOpenBuyOrders():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     orders = trading_client.get_orders(status='open', direction='desc', limit=100, nested=True)
     return orders
 
 def getOpenSellOrders():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     orders = trading_client.get_orders(status='open', direction='asc', limit=100, nested=True)
     return orders
 
 def cancelAllOrders():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     orders = trading_client.cancel_orders()
     return orders
 
 def getAllPositions():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     positions = trading_client.get_all_positions()
     return positions 
 
 def closeAllPositionsAndOrders():
-    trading_client = TradingClient(paper_apikey, paper_secretkey)
+    trading_client = TradingClient(apikey, secretkey)
     positions = trading_client.close_all_positions(cancel_orders=True)
     return positions
 
-
+def getHistoricalCryptoData(symbol, timeframe, start_date, end_date) ->pd.DataFrame:
+    client = CryptoHistoricalDataClient(apikey, secretkey)
+    request_params = CryptoBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=timeframe,
+        start_date=start_date,
+        end_date=end_date
+    )
+    historical_data = client.get_crypto_bars(request_params)
+    df = historical_data.df.loc[symbol]
+    df.columns = [s.capitalize() for s in df.columns]
+    return df[['Open', 'High', 'Low', 'Close']] 
 
 def getLatestCryptoQuote(symbol):
-    client = CryptoHistoricalDataClient()
+    client = CryptoHistoricalDataClient(apikey, secretkey)
     request_params = CryptoLatestQuoteRequest(symbol_or_symbols=symbol)
     latest_quote = client.get_crypto_latest_quote(request_params)
     return latest_quote
 
-import time
+# import time
 
-while True:
-    q = getLatestCryptoQuote('BTC/USD')['BTC/USD']
-    print(q.ask_price, q.bid_price)
-    time.sleep(5)
+# while True:
+#     q = getLatestCryptoQuote('BTC/USD')['BTC/USD']
+#     print(q.ask_price, q.bid_price)
+#     time.sleep(5
+
+# df = getHistoricalCryptoData(
+#     symbol='BTC/USD',
+#     timeframe=TimeFrame(1, TimeFrameUnit.Minute),
+#     start_date=datetime(2024,1,11),
+#     end_date=datetime(2024,1,12)
+# )
+
+# print(df.head())
+# print(df.tail())
     
